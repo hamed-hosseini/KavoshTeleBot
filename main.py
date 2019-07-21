18ea2b4711ee8b47bf25436ea8b8ac90ded5c324
Hamed <hellihdhs@gmail.com>
2019-07-21 21:18:40 +0430

    Adding inline butomn

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 token = "833040281:AAGZVzJCBFbMjnv1QnLYJYtnBSUlzsWjJS0"
+import os
 import telebot
 from telebot import types
 import pandas as pd
@@ -16,7 +17,8 @@ def command_start(message):
     bot.reply_to(message, 'سلام به کاوش سرام خوش آمدید')
     # print(message.from_user.username)
     if check_admin(message):
-        print('Admin Enterd')
+        if not os.path.exists('pictures'):
+            os.mkdir('pictures')
         main_menu_admin(message)
     elif check_co_worker(message):
         main_menu_client(message)
@@ -64,6 +66,9 @@ def main_menu_client_action(message):
         bot.send_message(message.chat.id, 'بلوار آزادگان - خیابان سهیل - کوچه حسینی - بازرگانی کاوش سرام')
         bot.send_location(message.chat.id, 35.610928, 51.351182)
         main_menu_client(message)
+    elif message.text == 'سبدخرید':
+        df = db_to_df('select * from sale_basket where user_ID="{0}"'.format(message.from_user.username))
+        show_sale_basket_client(message, df)
     else:
         bot.reply_to(message, 'گزینه دیگری انتخاب کنید')
         main_menu_client(message)
@@ -71,8 +76,10 @@ def existance_item_menue_admin(message):
     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, selective=True)
     item_existance = types.KeyboardButton('استعلام موجودی')
     item_sales_bascket = types.KeyboardButton('افزودن کالای جدید')
+    item_sales_exit_bascket = types.KeyboardButton('وارد کردن کالای فروش رفته')
     item_main_menu = types.KeyboardButton('منو اصلی')
     markup.row(item_existance, item_sales_bascket, item_main_menu)
+    markup.row(item_sales_exit_bascket)
     message = bot.reply_to(message, 'لطفا گزینه مورد نظر را انتخاب کنید', reply_markup=markup)
     bot.register_next_step_handler(message, existance_item_menue_admin_action)
 import pdfkit as pdf
@@ -83,18 +90,52 @@ def existance_item_menue_admin_action(message):
         df = db_to_df('select * from products')
         print(df)
         df.to_csv('Existance.csv')
-        show_df(message, df)
+        show_df_admin(message, df)
         with open('Existance.csv', 'r') as f:
             bot.send_document(message.chat.id, f)
         writeExcel(df, 'Existance','Existance')
         with open('Existance.xlsx', 'r') as f:
             bot.send_document(message.chat.id, f)
             existance_item_menue_admin(message)
+    elif message.text == 'وارد کردن کالای فروش رفته':
+        message = bot.reply_to(message, 'نام کالا را وارد کنید')
+        bot.register_next_step_handler(message,check_detail)
     elif message.text == 'منو اصلی':
         main_menu_admin(message)
     else:
         existance_item_menue_admin(message)
-
+def check_detail(message):
+    df = db_to_df('select * from products')
+    df_prod = df[df.name == message.text]
+    if df.empty:
+        bot.reply_to(message, 'این کالا موجود نمی باشد')
+        existance_item_menue_admin(message)
+    else:
+        show_df_admin(message, df_prod)
+        markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, selective=True)
+        item = []
+        for i in range(len(df_prod)):
+            item += [types.KeyboardButton(str(i + 1))]
+            markup.row(item[i])
+        message = bot.reply_to(message, 'ردیف مورد نظر را انتخاب کنید', reply_markup=markup)
+        bot.register_next_step_handler(message, edit_prod, df_prod)
+def edit_prod(message, df_prod):
+    id = df_prod.at[int(message.text) - 1, 'name'] + '-' +\
+         df_prod.at[int(message.text) - 1, 'size'] + '-' + \
+         df_prod.at[int(message.text) - 1, 'grade']
+    print(id)
+    message = bot.reply_to(message, 'تعداد کارتن را وارد کنید')
+    bot.register_next_step_handler(message, edit_carton_size, id)
+def edit_carton_size(message, id):
+    df = db_to_df('select * from products')
+    print(df)
+    new_df = df[df.ID == id]
+    print('new_df:', new_df)
+    existing_count = new_df.at[0, 'count_box']
+    df.set_value(df.ID == id, 'count_box', existing_count - int(message.text))
+    df_to_db(df, 'kavoshbot', 'products')
+    show_df_admin(message, df)
+    existance_item_menue_admin(message)
 def existance_item_search_client(message):
     message = bot.reply_to(message, 'نام کالای مورد نظر را وارد نمایید')
     bot.register_next_step_handler(message, existance_item_search_client_action)
@@ -104,7 +145,7 @@ def existance_item_search_client_action(message):
     if df.empty:
         message = bot.reply_to(message, 'کالایی با این نام موجود نمی باشد')
     else:
-        show_df(message, df)
+        show_df_client(message, df)
     main_menu_client(message)
 def add_name_admin(message):
     message = bot.reply_to(message, 'نام کالا را وارد کنید')
@@ -155,18 +196,31 @@ def add_box_count_menu_admin(message):
     message = bot.reply_to(message, 'تعداد کارتن را وارد کنید')
     bot.register_next_step_handler(message, add_box_count_menu_admin_action)
 def add_box_count_menu_admin_action(message):
-    product['count_box'] = message.text
+    product['count_box'] = int(message.text)
     add_box_price_menu_admin(message)
 def add_box_price_menu_admin(message):
     message = bot.reply_to(message, 'قیمت هر متر مربع(تومان) را وارد کنید')
     bot.register_next_step_handler(message, add_box_price_menu_admin_action)
 def add_box_price_menu_admin_action(message):
     product['price'] = message.text
+    add_image_menu_admin(message)
+def add_image_menu_admin(message):
+    message = bot.reply_to(message, 'عکس محصول را بارگذاری کنید')
+    bot.register_next_step_handler(message, add_image_menu_admin_action)
+def add_image_menu_admin_action(message):
+    print(message.photo[3].file_id)
+    product['file_id'] = message.photo[3].file_id
+    file = bot.get_file(message.photo[3].file_id)
+    downlaoded_file = bot.download_file(file.file_path)
+    with open(os.path.join(os.path.join(os.getcwd(), 'pictures'), message.photo[3].file_id + '.jpg'), 'wb') as f:
+        f.write(downlaoded_file)
+    product['ID'] = product['name'] + '-' + product['size'] + '-' + product['grade']
     try:
         df = db_to_df('select * from products')
     except:
         df = pd.DataFrame(data=product, index=[0])
         df_to_db(df, 'kavoshbot', 'products')
+        bot.reply_to(message, 'کالا با موفقیت ثبت گردید')
     else:
         if product['name'] not in df.name.values:
             df = df.append(product, ignore_index=True)
@@ -261,6 +315,7 @@ def db_to_df(query):
     db_connection = sqlEngine.connect()
 
     df = pd.read_sql(query, db_connection, index_col='index')
+    # df.drop(['file_id'], axis=1).reset_index(drop=True)
     db_connection.close()
     return df
 import os
@@ -278,19 +333,70 @@ def writeExcel(df,name, sheet):
     df.to_excel(writer, sheet_name=sheet)
     writer.save()
     writer.close()
-def show_df(message, df):
+def show_df_admin(message, df):
+    file_id = df['file_id']
+    ID = df['ID']
+    df = df.drop(['ID', 'file_id'], axis=1).reset_index(drop=True)
     all_prod = ''
     text = ''
     col = df.columns.tolist()
+    print(col)
     for i in range(len(df)):
         my_list = df.loc[i, :].tolist()
-        new_list = [prod_kw[col[j]] + ':' + my_list[j] for j in range(len(col))]
-        text = ' - '.join(new_list)
+        new_list = [str(prod_kw[col[j]]) + ':' + str(my_list[j]) for j in range(len(col))]
+        text = str(i+1) + ': ' + ' - '.join(new_list)
+        photo = open(os.path.join(os.path.join(os.getcwd(),'pictures'), str(file_id[i]) + '.jpg'), 'rb')
+        # keyboard = types.InlineKeyboardMarkup()
+        # item_add_to_sale = types.InlineKeyboardButton(text="افزودن به سبد خرید", callback_data='add_to_sale_basket_client-' + str(ID[i]))
+        # keyboard.add(item_add_to_sale)
+        bot.send_photo(message.chat.id, photo=photo, caption=text)
         all_prod += text + '\n'
-    bot.send_message(message.chat.id, text)
+def show_df_client(message, df):
+    file_id = df['file_id']
+    ID = df['ID']
+    df = df.drop(['ID', 'file_id'], axis=1).reset_index(drop=True)
+    all_prod = ''
+    text = ''
+    col = df.columns.tolist()
+    print(col)
+    for i in range(len(df)):
+        my_list = df.loc[i, :].tolist()
+        new_list = [str(prod_kw[col[j]]) + ':' + str(my_list[j]) for j in range(len(col))]
+        text = str(i+1) + ': ' + ' - '.join(new_list)
+        photo = open(os.path.join(os.path.join(os.getcwd(),'pictures'), str(file_id[i]) + '.jpg'), 'rb')
+        keyboard = types.InlineKeyboardMarkup()
+        item_add_to_sale = types.InlineKeyboardButton(text="افزودن به سبد خرید", callback_data='add_to_sale_basket_client-' + str(ID[i]))
+        keyboard.add(item_add_to_sale)
+        bot.send_photo(message.chat.id, photo=photo, caption=text, reply_markup=keyboard)
+        # bot.send_message(message.chat.id, "Я – сообщение из обычного режима", reply_markup=keyboard)
+        all_prod += text + '\n'
+def show_sale_basket_client(message, df):
+    # for i in range(len(df)):
+    #
+    #     prod_ID = df.at[i, 'product_ID']
+    pass
 
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    print(call)
+    if call.message:
+        if 'add_to_sale_basket_client-' in call.data:
+            message = bot.send_message(call.message.chat.id, 'تعداد کارتن را وارد کنید')
+            bot.register_next_step_handler(message, add_box_count_to_sale_basket_client, call)
+def add_box_count_to_sale_basket_client(message, call):
+    count = int(message.text)
+    prod_ID = call.data.split('add_to_sale_basket_client-')[1]
+    user_ID = '@' + call.from_user.username
+    try:
+        df_sale = db_to_df('select * from sale_basket')
+    except:
+        df_sale = pd.DataFrame(data={'user_ID': user_ID, 'prod_ID': prod_ID, 'count':count}, index=[0])
+        df_to_db(df_sale, 'kavoshbot', 'sale_basket')
+    else:
+        df_sale = df_sale.append({'user_ID': user_ID, 'prod_ID': prod_ID}, ignore_index=True)
+        df_to_db(df_sale, 'kavoshbot', 'sale_basket')
 def check_admin(message):
-    if message.from_user.username == 'HMDhosseini':
+    if message.from_user.username == 'mjhossaini':
         return True
     else:
         return False
